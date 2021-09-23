90c30030a6cfb5a99d9cf0a24c2db550eeda4b9b
mumu8848 <mumu88008@163.com>
2021-09-23 22:26:25 +0800

    测试:第一次发布信息

---
+++ TestForQi/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <Windows.h>
+
+int main(void) { 
+	char name; 
+	int pwd; 
+	std::cout << "ÇëÊäÈëÕËºÅ£º"; 
+	std::cin >> name;
+	std::cout << "ÇëÊäÈëÃÜÂë£º"; 
+	std::cin >> pwd; 
+	/*
+	std::cout << "1.ÍøÕ¾ 404 ¹¥»÷" << std::endl; 
+	std::cout << "2.ÍøÕ¾´Û¸Ä¹¥»÷" << std::endl;
+	std::cout << "3.ÍøÕ¾¹¥»÷¼ÇÂ¼" << std::endl;
+	std::cout << "4.DNS ¹¥»÷" << std::endl; 
+	std::cout << "5.·þÎñÆ÷ÖØÆô¹¥»÷" << std::endl; 
+	*/ 
+	system("pause"); 
+	return 0; 
+}
